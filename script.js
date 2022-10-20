02917faaf10fbb8ab8a2cf731bfdcb1fe24ebd32
Luke Lai <lukelai.dev@gmail.com>
2022-10-20 14:49:28 +0100

    populate coundown UI

--- script.js
+++ script.js
@@ -2,16 +2,54 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
+
 let countdownTitle = "";
 let countdownDate = "";
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
 
+// Populate Countdown / Complete UI
+const updateDOM = () => {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+
+  //   Popolate Countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${minutes}`;
+  timeElements[2].textContent = `${hours}`;
+  timeElements[3].textContent = `${seconds}`;
+
+  //   Hide Input
+  inputContainer.hidden = true;
+  // Show Countdown
+  countdownEl.hidden = false;
+};
+
 // Take Values from From input
 const updateCountdown = (e) => {
-  e.preventDafault();
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  //   Get number version of current Date, update DOM
+  countdownValue = new Date(countdownDate).getTime();
+  updateDOM();
 };
 
 // Event Listeners
