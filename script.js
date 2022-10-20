c0371effe699c11b965c927f50b94616d5c4735f
Luke Lai <lukelai.dev@gmail.com>
2022-10-20 14:33:46 +0100

    store title and date values from the from

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+// Take Values from From input
+const updateCountdown = (e) => {
+  e.preventDafault();
+};
+
+// Event Listeners
+countdownForm.addEventListener("submit", updateCountdown);
